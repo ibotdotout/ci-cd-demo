fc48f7caa83ec3d803fca447d02af8497461abbf
“teerasak” <teerasak@thoughtworks.com>
2019-10-19 13:02:16 +0700

    [Vue] add more unit tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -9,4 +9,22 @@ describe('HelloWorld.vue', () => {
     })
     expect(wrapper.text()).toMatch(msg)
   })
+
+  it('renders three h3 titles', () => {
+    const msg = ''
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    expect(wrapper.findAll('h3').length).toBe(3)
+  })
+
+  it('renders h3 with correct titles', () => {
+    const msg = ''
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    expect(wrapper.findAll('h3').at(0).text()).toBe('Installed CLI Plugins')
+    expect(wrapper.findAll('h3').at(1).text()).toBe('Essential Links')
+    expect(wrapper.findAll('h3').at(2).text()).toBe('Ecosystem')
+  })
 })
